84529e89ace30b744b130efa51965edafef04dca
Inkorrect-Code <ntongprofessional@gmail.com>
2023-12-12 09:12:16 -0500

    created and formatted the board

--- practice-for-ch-js-browser-ttt-main/practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,12 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+import Game from "../ttt_node/game.js"
+import View from "./ttt-view.js" // work w/o slash?
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  
+  // let ttt = document.getElementByClassName("ttt") old
+  let ttt = document.querySelector(".ttt")
+  let game = new Game()
+  let view = new View(game, ttt)
+  
 });
--- practice-for-ch-js-browser-ttt-main/practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,8 +1,28 @@
 class View {
   constructor(game, el) {
+    this.el = el
+    this.game = game
+    
+    this.setupBoard()
   }
   
   setupBoard() {
+    const ul = document.createElement("ul") 
+    
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        const li = document.createElement('li')
+        li.dataset.row = row
+        li.dataset.column = col
+
+        // li.classList.add('cell')
+
+        ul.appendChild(li)
+      }
+    }
+    this.el.appendChild(ul)
+    
+
   }
   
   handleClick(e) {
@@ -13,6 +33,9 @@ class View {
   
   handleGameOver() {
   }
+
 }
+
+
 
 export default View;
